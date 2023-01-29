ab8518f4b58c03da862ef4946392ec0ac1006a30
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-01-29 21:35:13 +0200

    add redux-toolkit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,33 +1,33 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { nanoid } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts } from 'redux/contacts/contactsSelector';
 import { addContacts } from 'redux/contacts/contactsSlice';
-import * as yup from 'yup';
+// import * as yup from 'yup';
 import css from './ContactForm.module.css';
 import { useState } from 'react';
 
-const initialValues = {
-  name: '',
-  number: '',
-};
+// const initialValues = {
+//   name: '',
+//   number: '',
+// };
 
-const schema = yup.object().shape({
-  name: yup
-    .string()
-    .matches(
-      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-    )
-    .required(),
-  number: yup
-    .string()
-    .matches(
-      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
-      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-    )
-    .required(),
-});
+// const schema = yup.object().shape({
+//   name: yup
+//     .string()
+//     .matches(
+//       /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+//       "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//     )
+//     .required(),
+//   number: yup
+//     .string()
+//     .matches(
+//       /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+//       'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+//     )
+//     .required(),
+// });
 
 export const ContactForm = () => {
   const [name, setName] = useState('');
@@ -36,7 +36,12 @@ export const ContactForm = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
 
-  const handleSubmit = () => {
+  const handleChange = ({ target: { name, value } }) => {
+    name === 'name' ? setName(value) : setNumber(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts.`);
       return;
@@ -48,26 +53,43 @@ export const ContactForm = () => {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={schema}
-      onSubmit={handleSubmit}
-    >
-      <Form className={css.form}>
-        <label className={css.label} htmlFor="name">
-          Name
-          <Field className={css.input} type="text" name="name" required />
-          <ErrorMessage className={css.error} name="name" component="p" />
-        </label>
-        <label className={css.label} htmlFor="number">
-          Number
-          <Field className={css.input} type="tel" name="number" required />
-          <ErrorMessage className={css.error} name="number" component="p" />
-        </label>
-        <button className={css.button} type="submit">
-          Add contact
-        </button>
-      </Form>
-    </Formik>
+    // <Formik
+    //   initialValues={initialValues}
+    //   validationSchema={schema}
+    //   onSubmit={handleSubmit}
+    // >
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label} htmlFor="name">
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleChange}
+        />
+      </label>
+      <label className={css.label} htmlFor="number">
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChange}
+          
+        />
+      </label>
+      <button className={css.button} type="submit">
+        Add contact
+      </button>
+    </form>
+    // </Formik>
   );
 };
