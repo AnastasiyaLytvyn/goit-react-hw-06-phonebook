101e34f362bde7db6d286ff62efd22806a6aff58
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-01-30 17:16:24 +0200

    add redux-toolkit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,33 +1,9 @@
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { nanoid } from '@reduxjs/toolkit';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from 'nanoid';
 import { getContacts } from 'redux/contacts/contactsSelector';
 import { addContacts } from 'redux/contacts/contactsSlice';
-// import * as yup from 'yup';
-import css from './ContactForm.module.css';
-import { useState } from 'react';
-
-// const initialValues = {
-//   name: '',
-//   number: '',
-// };
-
-// const schema = yup.object().shape({
-//   name: yup
-//     .string()
-//     .matches(
-//       /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-//       "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//     )
-//     .required(),
-//   number: yup
-//     .string()
-//     .matches(
-//       /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
-//       'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-//     )
-//     .required(),
-// });
+import { Button, Form, Input, Label } from './ContactForm.styled';
 
 export const ContactForm = () => {
   const [name, setName] = useState('');
@@ -40,7 +16,7 @@ export const ContactForm = () => {
     name === 'name' ? setName(value) : setNumber(value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts.`);
@@ -53,16 +29,10 @@ export const ContactForm = () => {
   };
 
   return (
-    // <Formik
-    //   initialValues={initialValues}
-    //   validationSchema={schema}
-    //   onSubmit={handleSubmit}
-    // >
-    <form className={css.form} onSubmit={handleSubmit}>
-      <label className={css.label} htmlFor="name">
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor="name">
         Name
-        <input
-          className={css.input}
+        <Input
           type="text"
           name="name"
           value={name}
@@ -71,11 +41,10 @@ export const ContactForm = () => {
           required
           onChange={handleChange}
         />
-      </label>
-      <label className={css.label} htmlFor="number">
+      </Label>
+      <Label htmlFor="number">
         Number
-        <input
-          className={css.input}
+        <Input
           type="tel"
           name="number"
           value={number}
@@ -83,13 +52,9 @@ export const ContactForm = () => {
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={handleChange}
-          
         />
-      </label>
-      <button className={css.button} type="submit">
-        Add contact
-      </button>
-    </form>
-    // </Formik>
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
   );
 };
